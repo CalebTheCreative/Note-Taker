90c2d339ac269b8fb487a494181c6c239fdb0d43
Caleb Hopkins <CalebHopkins@outlook.com>
2021-01-12 11:25:47 -0600

    Added HTML routes.

--- server.js
+++ server.js
@@ -10,3 +10,12 @@ const PORT = 3001;
 // Lets Express use data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// HTML Routes
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
