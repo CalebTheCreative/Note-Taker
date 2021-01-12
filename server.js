c50300019e360b821e8458af3221b15c60d92011
Caleb Hopkins <CalebHopkins@outlook.com>
2021-01-12 10:27:34 -0600

    Added dependencies and Express server.

---
+++ server.js
@@ -0,0 +1,8 @@
+// Dependencies
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+// Starts Express server
+const app = express();
+const PORT = 3001;
